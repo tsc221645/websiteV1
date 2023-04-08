30749f403959cabf33216174ae6870b4898d6986
tsc221645 <tsc221645@uvg.edu.gt>
2023-04-07 21:11:19 -0600

    homepage and about

---
+++ website-v1/src/components/About.vue
@@ -0,0 +1,17 @@
+<!--HTML Section of About-->
+<template>
+<div>
+
+</div>
+</template>
+
+
+<!--Script Section of About-->
+<script>
+
+</script>
+
+<!--CSS Section of About-->
+<style scoped>
+
+</style>
---
+++ website-v1/src/components/Homepage.vue
@@ -0,0 +1,17 @@
+<!--HTML Section of Homepage-->
+<template>
+<div>
+
+</div>
+</template>
+
+
+<!--Script Section of Homepage-->
+<script>
+
+</script>
+
+<!--CSS Section of Homepage-->
+<style scoped>
+
+</style>
